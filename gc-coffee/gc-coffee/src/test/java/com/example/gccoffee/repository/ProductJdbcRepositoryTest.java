55e817aec2eae0f5d7451d5c9a7362f9e56d5a25
UJ15 <dbwogml15@naver.com>
2022-04-27 13:12:02 +0900

    :white_check_mark: test : test추가

---
+++ gc-coffee/gc-coffee/src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v8_0_11;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+class ProductJdbcRepositoryTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        var config= aMysqldConfig(v8_0_11)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order-mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+
+    }
+
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+
+    @Autowired
+    ProductRepository repository;
+
+    private static Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+
+    @Test
+    @Order(1)
+    @DisplayName("상풍을 추가할 수 있다.")
+    void testInsert() {
+        repository.insert(newProduct);
+        var all = repository.findAll();
+        assertThat(all.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("상풍을 이름으로 조회할 수 있다.")
+    void testFindByName() {
+        var product = repository.findByName(newProduct.getProductName());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품을 아이디로 조회할 수 있다.")
+    void testFindById() {
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("상풍들을 카테코리로 조회할 수 있다.")
+    void testFindByCategory() {
+        var product = repository.findByCategory(Category.COFFEE_BEAN_PACKAGE);
+        assertThat(product.isEmpty(), is(false));
+    }
+
+}
